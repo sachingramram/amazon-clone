4e4a5a23fe04c77ccb8a9498c4a445f3a4209f70
Sachin Pal <sachinpal9352@gmial.com>
2025-09-16 16:58:15 +0530

    first amazon3

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: { ignoreDuringBuilds: true },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
